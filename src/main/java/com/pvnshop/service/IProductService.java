4a913b0fd89d4563f606c2358a1de94fc7a545b7
Thanh Phong <thanh2652003abc@gmail.com>
2023-11-15 15:07:48 +0700

    Modify

--- src/main/java/com/pvnshop/service/IProductService.java
+++ src/main/java/com/pvnshop/service/IProductService.java
@@ -7,9 +7,9 @@ import com.pvnshop.models.ProductModel;
 public interface IProductService {
 	public List<ProductModel> findTop3();
 	public List<ProductModel> findRateTop3();
-  List<ProductModel> findall();
+	List<ProductModel> findAll();
 	ProductModel findByID(int id);
-  List<ProductModel> findProductByCate(int cateId);
+	List<ProductModel> findProductByCate(int cateId);
 	int CountProductByCate(int cateId);
 }
 
